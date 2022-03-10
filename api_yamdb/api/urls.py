d14a0fb58e4fee1a95143f4e69973bd8f34f01ff
Дмитрий <dmitry.osipov@moex.com>
2022-03-10 14:01:53 +0300

    Спринт9

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,10 +2,18 @@ from django.urls import include, path
 from rest_framework import routers
 
 from .utils import generate_and_send_confirmation_code_to_email
-from .views import APISignUp, APIToken, UserViewSet
+from .views import (APISignUp, APIToken, UserViewSet, ReviewViewSet,
+                    CommentViewSet)
 
 router = routers.DefaultRouter()
 router.register('users', UserViewSet, basename='User')
+router.register(
+    r'titles/reviews',
+    ReviewViewSet, basename='reviews')
+router.register(
+    r'titles/reviews/comments',
+    CommentViewSet, basename='comments')
+
 
 urlpatterns = [
     path('v1/', include(router.urls)),
