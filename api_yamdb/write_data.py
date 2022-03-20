332dcfd27d87a45da4f83478dd2e5811f78c0bbe
Raidzin <60663550+Raidzin@users.noreply.github.com>
2022-03-20 17:00:50 +0300

    поправил скрипт наполнения базы данных

--- api_yamdb/write_data.py
+++ api_yamdb/write_data.py
@@ -7,10 +7,10 @@ cur = conn.cursor()
 os.chdir('static')
 os.chdir('data')
 
-sql_file = open('db.sqlite3.sql', 'r')
-while True:
-    line = sql_file.readline()
-    if not line:
-        break
-    cur.execute(line.strip())
+sql_file = open('db.sqlite3.sql', 'r', encoding='utf-8')
+inserts = sql_file.read()
+sql_file.close()
+inserts = inserts.split(';')
+for insert in inserts:
+    cur.execute(insert)
 sql_file.close()
