23af772897c6ba4f0d91220e928924c3cf8c6558
Raidzin <60663550+Raidzin@users.noreply.github.com>
2022-03-26 01:22:26 +0300

    исправил валидатор

--- api_yamdb/api/utils.py
+++ api_yamdb/api/utils.py
@@ -4,21 +4,22 @@ from reviews.models import User
 
 RESERVED_NAME = 'me'
 RESERVED_NAME_ERROR = 'Имя пользователя "me" использовать нельзя.'
-USER_OR_NAME_REGISTERED = 'Пользователь с таким именем уже зарегестрирован.'
-USER_OR_EMAIL_REGISTERED = 'Пользователь с такой почтой уже зарегестрирован.'
+USER_OR_NAME_REGISTERED = 'Пользователь с таким именем уже зарегистрирован.'
+USER_OR_EMAIL_REGISTERED = 'Пользователь с такой почтой уже зарегистрирован.'
 INVALID_EMAIL = 'Введите корректный email.'
 
 
-def validate_username(value):
-    if value == RESERVED_NAME:
+def validate_username(username):
+    if username == RESERVED_NAME:
         raise ValidationError(RESERVED_NAME_ERROR)
-    return value
+    return username
 
 
 def email_validate(email):
     try:
         if not User.objects.filter(email=email).exists():
             validate_email(email)
+            return email
     except ValidationError:
         raise ValidationError(
             {'email': INVALID_EMAIL}
