addab1c2f4173e73cb84641e9d46e6285ea8a982
Raidzin <60663550+Raidzin@users.noreply.github.com>
2022-03-26 17:35:56 +0300

    поправил максимальную длинну

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -23,7 +23,7 @@ class User(AbstractUser):
         (USER, USER)
     )
     role = models.CharField(
-        max_length=len(max(ROLES, key=lambda role: len(role[0]))),
+        max_length=max(len(role) for role, _ in ROLES),
         choices=ROLES,
         default=USER,
         verbose_name='Роль',
