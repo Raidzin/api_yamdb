829cf0bae38a5a302c52594bd42d719d2e9d06f3
Raidzin <60663550+Raidzin@users.noreply.github.com>
2022-03-26 20:16:57 +0300

    исправил скрипт записи данных x2

--- api_yamdb/parse_csv.py
+++ api_yamdb/parse_csv.py
@@ -4,37 +4,53 @@ import csv
 
 INSERT = "INSERT INTO {0} ({1}) VALUES ({2});"
 
-conn = sqlite3.connect('db.sqlite3')
-cur = conn.cursor()
 
-os.chdir('static')
-os.chdir('data')
+def get_columns_and_values(row):
+    into = list(row.keys())
+    values = list(row.values())
+    for i, value in enumerate(values):
+        value = value.replace("'", "''")
+        values[i] = f"'{value}'"
+    return into, values
 
-file_names = [file for file in os.listdir() if file.endswith('.csv')]
 
-for file_name in file_names:
-    table_name = 'reviews_' + file_name[:-4]
-    with open(file_name, 'r', encoding='utf-8') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            into = list(row.keys())
-            values = list(row.values())
-            for i, value in enumerate(values):
-                value = value.replace("'", "''")
-                values[i] = f"'{value}'"
+def add_user_system_data(into, values):
+    into.extend(('password', 'is_superuser',
+                 'is_staff', 'is_active', 'date_joined'))
+    values.extend(('0', '0', '0', '0', '0'))
+    return into, values
 
-            if table_name == 'reviews_user':
-                into.extend(('password', 'is_superuser',
-                             'is_staff', 'is_active', 'date_joined'))
-                values.extend(('0', '0', '0', '0', '0'))
-            try:
-                into = ', '.join(into)
-                values = ', '.join(values)
-                cur.execute(INSERT.format(table_name, into, values))
-            except Exception as error:
-                print()
-                print(INSERT.format(table_name, into, values))
-                print(error, end='\n\n')
-cur.execute('COMMIT;')
 
-# Хорошо что скрип записи данных не проходит ревью...
+def write_data(cur, table_name, into, values):
+    cur.execute(
+        INSERT.format(
+            table_name,
+            ', '.join(into),
+            ', '.join(values)
+        )
+    )
+
+
+def parse_csvs(file_names, cur):
+    for file_name in file_names:
+        table_name = 'reviews_' + file_name[:-4]
+        with open(file_name, 'r', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                into, values = get_columns_and_values(row)
+                if table_name == 'reviews_user':
+                    into, values = add_user_system_data(into, values)
+                write_data(cur, table_name, into, values)
+    cur.execute('COMMIT;')
+
+
+if __name__ == '__main__':
+    conn = sqlite3.connect('db.sqlite3')
+    cur = conn.cursor()
+
+    os.chdir('static')
+    os.chdir('data')
+
+    file_names = [file for file in os.listdir() if file.endswith('.csv')]
+
+    parse_csvs(file_names, cur)
